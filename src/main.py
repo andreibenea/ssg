1340d4898769b145fa64593c3440a720995caf02
Andrei Benea <andrei.benea89@gmail.com>
2025-11-11 00:14:54 -0500

    copy static

--- src/main.py
+++ src/main.py
@@ -1,7 +1,38 @@
-from textnode import TextNode
+import os
+import shutil
+
+
+def generate_public():
+    public = "./public"
+    if os.path.exists(public):
+        print("PATH EXISTS... removing")
+        shutil.rmtree(public)
+    copy_static("./static", "./public")
+
+
+def copy_static(path, destination):
+    print(f"CURRENT PATH: {path}")
+    print(f"CURRENT DESTINATION: {destination}")
+    if os.path.exists(path):
+        print(f"ON PATH: {path}")
+        static_contents = os.listdir(path)
+        print(static_contents)
+    for item in static_contents:
+        if str(item).startswith("."):
+            continue
+        new_path = os.path.join(path, item)
+        new_destination = os.path.join(destination, item)
+        print(f"NEW DESTINATION: {new_destination}")
+        if os.path.isdir(new_path):
+            os.makedirs(f"{new_destination}")
+            copy_static(new_path, new_destination)
+        if os.path.isfile(new_path):
+            os.makedirs(os.path.dirname(new_destination), exist_ok=True)
+            shutil.copy(new_path, new_destination)
+
+
 def main():
-    text_node = TextNode("This is testing test", "text", "https://www.boot.dev")
-    print(text_node)
+    generate_public()
 
 
 if __name__ == "__main__":
